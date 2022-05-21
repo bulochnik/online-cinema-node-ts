397860d385718ac8a581b4367e1767fc75d99eec
bulochnik <kukushkaukurushka@gmail.com>
2022-05-21 13:02:52 +0300

    Add actor features

--- src/actor/actor.controller.ts
+++ src/actor/actor.controller.ts
@@ -4,7 +4,6 @@ import {
 	Delete,
 	Get,
 	HttpCode,
-	NotFoundException,
 	Param,
 	Post,
 	Put,
@@ -15,43 +14,35 @@ import {
 
 import { IdValidationPipe } from './../pipes/id.validation.pipe';
 import { Auth } from '../auth/decorators/auth.decorator';
-import { CreateGenreDto } from './dto/create-genre.dto';
-import { GenreService } from './genre.service';
+import { ActorService } from './actor.service';
+import { ActorDto } from './dto/actor.dto';
 
-@Controller('genres')
-export class GenreController {
-	constructor(private readonly genreService: GenreService) {}
+@Controller('actors')
+export class ActorController {
+	constructor(private readonly actorService: ActorService) {}
 
 	@Get('by-slug/:slug')
 	async bySlug(@Param('slug') slug: string) {
-		const genre = await this.genreService.bySlug(slug);
-		if (!genre) throw new NotFoundException('Slug not found');
-
-		return genre;
-	}
-
-	@Get('collections')
-	async getCollections() {
-		return this.genreService.getCollections();
+		return this.actorService.bySlug(slug);
 	}
 
 	@Get()
 	async getAll(@Query('searchTerm') searchTerm?: string) {
-		return this.genreService.getAll(searchTerm);
+		return this.actorService.getAll(searchTerm);
 	}
 
 	@Get(':id')
 	@Auth('admin')
 	async get(@Param('id', IdValidationPipe) id: string) {
-		return this.genreService.byId(id);
+		return this.actorService.byId(id);
 	}
 
-	@UsePipes(new ValidationPipe())
+	@UsePipes()
 	@Post()
 	@HttpCode(200)
 	@Auth('admin')
 	async create() {
-		return this.genreService.create();
+		return this.actorService.create();
 	}
 
 	@UsePipes(new ValidationPipe())
@@ -60,15 +51,15 @@ export class GenreController {
 	@Auth('admin')
 	async update(
 		@Param('id', IdValidationPipe) id: string,
-		@Body() dto: CreateGenreDto,
+		@Body() dto: ActorDto,
 	) {
-		return this.genreService.update(id, dto);
+		return this.actorService.update(id, dto);
 	}
 
 	@Delete(':id')
 	@HttpCode(200)
 	@Auth('admin')
 	async delete(@Param('id', IdValidationPipe) id: string) {
-		return this.genreService.delete(id);
+		return this.actorService.delete(id);
 	}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { GenreModule } from './genre/genre.module';
 import { FileModule } from './file/file.module';
+import { ActorModule } from './actor/actor.module';
 
 @Module({
 	imports: [
@@ -23,6 +24,7 @@ import { FileModule } from './file/file.module';
 		UserModule,
 		GenreModule,
 		FileModule,
+		ActorModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
